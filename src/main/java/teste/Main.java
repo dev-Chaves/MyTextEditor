f9f2dc5cb1a4054e657df18f62e15c174f3fc3c3
chaves <chavesprogrammer@gmail.com>
2025-04-08 12:13:37 -0300

    estudos da criação de interface

---
+++ src/main/java/teste/Main.java
@@ -0,0 +1,13 @@
+package teste;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame janela = new JFrame();
+        janela.setVisible(true);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
