4ae34cb7ccc1db26ceb844cf404a4d8ff05637ee
chaves <chavesprogrammer@gmail.com>
2025-04-09 12:01:08 -0300

    brincadeiras e testando

--- src/main/java/teste/Main.java
+++ src/main/java/teste/Main.java
@@ -1,7 +1,6 @@
 package teste;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -18,20 +17,35 @@ public class Main {
         menu.setMnemonic(KeyEvent.VK_A);
         menu.getAccessibleContext().setAccessibleDescription("This is a menu");
 
-        JMenuItem teste = new JMenuItem("Teste Rápido");
+        JMenuItem newFile = new JMenuItem("New");
+        JMenuItem openFile = new JMenuItem("Open");
+        JMenuItem closeFile = new JMenuItem("Close");
+        JMenuItem testes = new JMenuItem("Testes");
 
         menubar.add(menu);
-        menu.add(teste);
+        menu.add(newFile);
+        menu.add(openFile);
+        menu.add(closeFile);
+        menu.add(testes);
 
-        teste.addActionListener(new ActionListener() {
+        newFile.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Teste Rápido");
+                System.out.println("Novo arquivo!");
             }
         });
 
-        JTextArea TextArea = new JTextArea();
+        JTextArea textArea = new JTextArea();
 
+        textArea.setLineWrap(true);
+
+        testes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                var lines = textArea.getLineCount();
+                System.out.println("Total de linhas: " + lines);
+            }
+        });
 
         JFrame frame = new JFrame();
 
@@ -39,9 +53,8 @@ public class Main {
 
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         frame.setJMenuBar(menubar);
-        frame.add(TextArea);
+        frame.add(textArea);
 
     }
 }
---
+++ src/main/java/tutorial/Main.java
@@ -0,0 +1,7 @@
+package tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
